450fe66c253c46b528bc438bfda1244b7ea417ab
Ethens <prashast.rastogi@gmail.com>
2017-10-29 13:21:44 +0530

    created basic layouts :

--- app/src/main/java/ethens/playo/activity/DashboardActivity.java
+++ app/src/main/java/ethens/playo/activity/DashboardActivity.java
@@ -1,12 +1,13 @@
-package ethens.playo;
+package ethens.playo.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import ethens.playo.R;
 
-public class MainActivity extends AppCompatActivity {
+public class DashboardActivity extends AppCompatActivity {
 
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_main);
+    setContentView(R.layout.activity_dashboard);
   }
 }
